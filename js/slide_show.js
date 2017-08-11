4f4d5067a2ad03346dbc57751fce14b08f95b644
fusionevents <30903645+fusionevents@users.noreply.github.com>
2017-08-12 00:11:46 +0530

    Update slide_show.js

--- js/slide_show.js
+++ js/slide_show.js
@@ -1,14 +1,18 @@
-var slideIndex = 0;
+ var slideIndex = 0;
 showSlides();
 
-function showSlides() {
-    var c;
+function showSlides() {   
     var slides = document.getElementsByClassName("mySlides");
-    for (c = 0; c < slides.length; c++) {
-        slides[c].style.display = "none"; 
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";  
     }
     slideIndex++;
-    if (slideIndex> slides.length) {slideIndex = 1} 
-    slides[slideIndex-1].style.display = "block";
-    setTimeout(showSlides, 4000); // Change image every 2 seconds
+    if (slideIndex> slides.length) {slideIndex = 1}    
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
