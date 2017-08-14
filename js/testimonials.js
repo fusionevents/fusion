cc91f435351debd150788bc88b95f4a0f94684ce
fusionevents <30903645+fusionevents@users.noreply.github.com>
2017-08-14 20:22:03 +0530

    Update testimonials.js

--- js/testimonials.js
+++ js/testimonials.js
@@ -5,11 +5,12 @@ $(document).ready(function(){
     $("#testimonial-slider").owlCarousel({
         items:1,
         itemsDesktop:[1000,1],
-        itemsDesktopSmall:[990,1],
+        itemsDesktopSmall:[979,1],
         itemsTablet:[768,1],
-        itemsMobile:[650,1],
         pagination:true,
-        navigation:false,
+        navigation:true,
+        navigationText:["",""],
+        slideSpeed:1000,
         autoPlay:true
     });
 });
